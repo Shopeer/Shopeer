3f446f932828289346422c7e333a0af328fffadc
nicolas-source <nzheng8@student.ubc.ca>
2022-07-01 18:28:56 -0700

    roomInsertOne works

--- backend/server.js
+++ backend/server.js
@@ -39,69 +39,172 @@ app.post("/", (req, res) => {
     res.send(req.body.text)
 })
 
+//-------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------
 
-let obj_user1 = {username: "test_user1"}
-let obj_user2 = {username: "test_user2"}
+// let obj_user1 = {username: "test_user1"}
+// let obj_user2 = {username: "test_user2"}
+// let obj_chat1 = {chatname: "test_chatname1", chathistory: null}
+// let obj_chat2 = {chatname: "test_chatname2", chathistory: null}
 
-let obj_chat1 = {chatname: "test_chatname1", chathistory: null}
-let obj_chat2 = {chatname: "test_chatname2", chathistory: null}
 
 
-app.post("/insertOne", async (req, res) => {
+//-------------------------------------------------------------------------------
+// Database Module: User Collection Submodule
+
+// Add New User POST https://shopeer.com/userDB/newUser
+// Creates and adds a new user into the User Database along with a new user_id
+// Body: User gmail
+// Response: New user_id, new user id token
+// Check authorization POST https://shopeer.com/userDB/auth
+// Checks authorization of user
+// Body: User Id Token AND User details
+// Response: Pass/fail
+// Retrieve User GET https://shopeer.com/userDB?user_id=[id]
+// Retrieves user from User Database with user_id
+// Body: User Id Token AND User details
+// Response: Success/Fail
+// Retrieve Queried User List GET https://shopeer.com/userDB?searchQuery=[query] 
+// Retrieves list of users objects from User Database that match the  query parameter
+// Param: query
+// Body: User Id Token
+// Response: user object list
+// Add New Search POST https://shopeer.com/userDB/newSearch
+// Adds new search for the user in User Collection
+// Body: user_id, location, distance, activity, budget
+// Reponse: success/fail
+// Update Profile POST https://shopeer.com/userDB/updateProfile?user_id=[id]
+// Updates user object in the User Collection with the new profile information
+// Param: user_id
+// Body: user id token AND New profile info {profile_pic, name, bio}
+// Response: success/fail
+// Remove Peer DELETE https://shopeer.com/userDB/removePeer
+// Deletes specified peer from the peers list of the user
+// Body: user id token, user_id, peer_id
+// Response:success / fail
+// Add Sent Invitation POST https://shopeer.com/userDB/addInvite
+// Adds the invitation sent by the user in user’s “sent invitation” list and in peer’s “recieved invitation” list
+// Body: user id token, user_id, peer_id
+// Response:success / fail
+// Decline Invitation POST https://shopeer.com/userDB/declineInvite
+// Declines an invite from a peer by removing the invite from user's “recieved invitation” list and peer’s “sent invitation” list
+// Body: user id token, user_id, peer_id
+// Response:success / fail
+// Accept Invitation POST https://shopeer.com/userDB/acceptInvite
+// Accepts an invite from a peer by removing the invite from user's “received invitation” list and peer’s “sent invitation” list, and adding peer_id to “peers ids” list
+// Body: user id token, user_id, peer_id
+// Response:success / fail
+
+//-------------------------------------------------------------------------------
+// Database Module: Room Collection Submodule
+
+// Add Chatroom PUT https://shopeer.com/roomDB
+// Adds a chatroom with the list of users specified in the Room Collection, and returns a room_id if action was successful
+// Body: User Id Token, list of user_ids
+// Response: room_id
+app.post("/roomInsertOne", async (req, res) => {
     try {
-        await mongoClient.db("shopeer_database").collection("chat_collection").insertOne(req.body)
-        res.status(200).send("item added successfully\n")
+        var result_debug = await mongoClient.db("shopeer_database").collection("room_collection").insertOne(req.body)
+        var objectId = req.body._id; 
+        res.status(200).send(objectId)
+        // console.log(result_debug)
     }
     catch (err) {
         console.log(err)
-        res.send(400).send(rr)
+        res.send(400).send(err)
     }
 })
 
-app.put("/replaceOne", async (req, res) => {
+
+// Get All Chatrooms GET https://shopeer.com/roomDB
+// Gets all chat rooms that a particular user is in.
+// Body: User Id Token AND User details
+// Response: List of room ids
+
+
+// Post message POST  https://shopeer.com/roomDB/message?room_id=[room_id]
+// Send a chat message to peer/ group
+// Param: Chat Id
+// Body:  user id token AND Message
+// Response: success/fail
+
+app.post("/messageInsertOne", async (req, res) => {
     try {
-        await mongoClient.db("shopeer_database").collection("chat_collection").replaceOne({ "task": 'Tutorial' }, req.body)
-        res.status(200).send("item modified successfully\n")
+        var result_debug = await mongoClient.db("shopeer_database").collection("room_collection").insertOne(req.body)
+        var objectId = req.body._id; 
+        res.status(200).send(objectId)
+        // console.log(result_debug)
     }
     catch (err) {
         console.log(err)
-        res.send(400).send(rr)
+        res.send(400).send(err)
+    }
+})
+
+
+// Remove Chatroom DELETE https://shopeer.com/roomDB?room_id=[room_id]
+// Param: room_id
+// Response: success/fail
+
+
+
+
+
+
+
+
+
+
+app.put("/replaceOne", async (req, res) => {
+    try {
+        const result_debug = await mongoClient.db("shopeer_database").collection("room_collection").replaceOne({ "task": 'Tutorial' }, req.body)
+        res.status(200).send("item modified successfully\n")
+        console.log(result_debug)
+    }
+    catch (err) {
+        console.log(err)
+        res.send(400).send(err)
     }
 })
 
 app.delete("/deleteOne", async (req, res) => {
     try {
-        await mongoClient.db("shopeer_database").collection("chat_collection").deleteOne({ "task": req.body.task })
+        await mongoClient.db("shopeer_database").collection("room_collection").deleteOne({ "task": req.body.task })
         res.status(200).send("item deleted successfully\n")
     }
     catch (err) {
         console.log(err)
-        res.send(400).send(rr)
+        res.send(400).send(err)
     }
 })
 
 app.get("/find", async (req, res) => {
     try {
-        const result = await mongoClient.db("shopeer_database").collection("chat_collection").find(req.body)
+        const result = await mongoClient.db("shopeer_database").collection("room_collection").find(req.body)
         await result.forEach(console.dir)
         res.send("items retrieved\n")
     }
     catch (err) {
         console.log(err)
-        res.send(400).send(rr)
+        res.send(400).send(err)
     }
 })
+
+//-------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------
 
 // For debug only
 app.get("/test", async (req, res) => {
   try {
-      const result_debug = await mongoClient.db("shopeer_database").collection("chat_collection").find({}).toArray()
+      const result_debug = await mongoClient.db("shopeer_database").collection("room_collection").find({}).toArray()
       console.log(result_debug[0]["testkey"])
       res.send(result_debug[0]["testkey"] + "\n");
   }
   catch (err) {
       console.log(err)
-      res.send(400).send(rr)
+      res.send(400).send(err)
   }
 })
 
@@ -113,7 +216,7 @@ app.get("/server_ip", async (req, res) => {
   }
   catch (err) {
       console.log(err)
-      res.send(400).send(rr)
+      res.send(400).send(err)
   }
 })
 
@@ -128,7 +231,7 @@ app.get("/server_time", async (req, res) => {
   }
   catch (err) {
       console.log(err)
-      res.send(400).send(rr)
+      res.send(400).send(err)
   }
 })
 
@@ -152,9 +255,9 @@ async function run() {
 
   // Ensure that a test document exists in database
   try {
-      count_result = await mongoClient.db("shopeer_database").collection("chat_collection").countDocuments({ testkey: "testdefinition" })
+      count_result = await mongoClient.db("shopeer_database").collection("room_collection").countDocuments({ testkey: "testdefinition" })
       if (count_result < 1) {
-          const insertResult = await mongoClient.db("shopeer_database").collection("chat_collection").insertOne({ testkey: "testdefinition" })
+          const insertResult = await mongoClient.db("shopeer_database").collection("room_collection").insertOne({ testkey: "testdefinition" })
           console.log(insertResult)
       }
   } catch (err) {
