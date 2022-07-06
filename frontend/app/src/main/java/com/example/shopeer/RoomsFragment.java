700abc3718e1ac8ee876a81ef2459d3bc18e123a
Farrandi Hernando <farrandi19@gmail.com>
2022-07-05 17:37:53 -0700

    FE3: Rooms UI Page (#30)
    * Added splashscreen + fixed profile fragment showing issue
    
    * Made UI layouts for the chatrooms
    
    * Finished the Chat Room Fragment Page
    
    * refactored some names from peer->room
    
    Co-authored-by: Farrandi Hernando

--- frontend/app/src/main/java/com/example/shopeer/RoomsFragment.java
+++ frontend/app/src/main/java/com/example/shopeer/RoomsFragment.java
@@ -3,10 +3,14 @@ package com.example.shopeer;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,15 +18,10 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class RoomsFragment extends Fragment {
+    final static String TAG = "RoomsFragment";
+    RecyclerView recyclerView;
+    private ArrayList<RoomObject> roomList;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public RoomsFragment() {
         // Required empty public constructor
@@ -32,33 +31,42 @@ public class RoomsFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment PeersFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static RoomsFragment newInstance(String param1, String param2) {
+    public static RoomsFragment newInstance() {
         RoomsFragment fragment = new RoomsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_rooms, container, false);
+        View v =  inflater.inflate(R.layout.fragment_rooms, container, false);
+
+        // initialize recycler view
+        roomList = new ArrayList<>();
+        recyclerView = v.findViewById(R.id.recyclerView);
+
+        // fetch data of peers and add to peerList
+        for (int i=0; i < 20; i++) {
+            String name = "Peer Number " + i;
+            roomList.add(new RoomObject(name,
+                    "Last Message sent by this peer",
+                    "00:00",
+                    R.drawable.temp_profile));
+        }
+
+        RecyclerAdapter recyclerAdapter = new RecyclerAdapter(roomList);
+        recyclerView.setAdapter(recyclerAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        return v;
     }
 }
