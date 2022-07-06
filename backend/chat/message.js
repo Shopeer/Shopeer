79fb50a0918acf4d4a04ee67d2243d318b9a349b
Grace <gracemyzhang@gmail.com>
2022-07-06 15:44:26 -0700

    fixed message id

--- backend/chat/message.js
+++ backend/chat/message.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const router = express.Router();
 const uri = "mongodb://127.0.0.1:27017"
@@ -16,9 +15,10 @@ const coll = client.db("shopeer_database").collection("room_collection")
  * Body:  sender email AND message text AND send time
  * Response: message id
 */
-//curl -X "POST" -H "Content-Type: application/json" -d '{"email": "nick@gmail.com", "text": "sup!", "time": "rn"}' localhost:8081/chat/message?room_id=62c4b6f309f37ad6591198b7
+//curl -X "POST" -H "Content-Type: application/json" -d '{"email": "nick@gmail.com", "text": "sup!", "time": "rn"}' localhost:8081/chat/message?room_id=62c4d5c76896713a30649546
 router.post("/", async (req, res) => {
     try {
+        var mssgid = ObjectId();
         var doc = await coll.updateOne(
             // searches for a document with the following fields
             {_id: ObjectId(req.query.room_id)},
@@ -26,6 +26,7 @@ router.post("/", async (req, res) => {
             {$push: {
                 "chathistory": 
                     {
+                        "mssg_id": mssgid,
                         "email": req.body.email,
                         "text": req.body.text,
                         "time": req.body.time // frontend will send as long
@@ -33,7 +34,7 @@ router.post("/", async (req, res) => {
                 }
             }
         )
-        console.log("\n mssg from " + req.body.email + " added to group chat with id " + doc.insertedId)
+        console.log("\n mssg from " + req.body.email + " added to group chat with id " + mssgid)
         res.status(200).send(doc.insertedId)
     } catch (err) {
         console.log(err)
