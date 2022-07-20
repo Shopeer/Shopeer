19332556d03960c38a7db2e36da425d099f7bd3c
sallyyjan <sallyyujeongan@gmail.com>
2022-07-19 21:03:03 -0700

    EditSearchActivity.java codeacy fix

--- frontend/app/src/main/java/com/example/shopeer/EditSearchActivity.java
+++ frontend/app/src/main/java/com/example/shopeer/EditSearchActivity.java
@@ -24,7 +24,6 @@ import com.android.volley.toolbox.Volley;
 import com.google.android.gms.common.api.Status;
 import com.google.android.libraries.places.api.Places;
 import com.google.android.libraries.places.api.model.Place;
-import com.google.android.libraries.places.api.net.PlacesClient;
 import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
 import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
 
