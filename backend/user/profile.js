018c68cee50643df83d8d2c5ba60f0a1b9863bf1
Grace <gracemyzhang@gmail.com>
2022-07-21 15:54:10 -0700

    commented out match

--- backend/user/profile.js
+++ backend/user/profile.js
@@ -1,12 +1,11 @@
 
-const http = require('http');
+require('http');
 var express = require("express")
-const { IPv4 } = require("ipaddr.js")
-const app = express()
+express()
 
 const user_profile_router = express.Router()
 
-const { MongoClient, ObjectId } = require("mongodb")  // this is multiple return
+const { MongoClient } = require("mongodb")  // this is multiple return
 const uri = "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.5.0"
 const mongoClient = new MongoClient(uri)
 
