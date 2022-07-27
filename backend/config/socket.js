ebf2d4f4f8b201558bdd9afdd537333a3fe93252
Farrandi Hernando <farrandi@Nandos-MacBook.local>
2022-07-26 20:40:59 -0700

    refactored port number

--- backend/config/socket.js
+++ backend/config/socket.js
@@ -1,10 +1,11 @@
 const SocketServer = require("websocket").server;
 const http = require("http");
+const PORT = 8000;
 
 const server = http.createServer((req, res) => {});
 
-server.listen(8000, () => {
-  console.log("Listening on port 3000...");
+server.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
 });
 
 wsServer = new SocketServer({ httpServer: server });
