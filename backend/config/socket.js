532e72082a6835817cb232bba8a3a77da4e00794
Farrandi Hernando <farrandi@Nandos-MacBook.local>
2022-07-26 20:33:28 -0700

    added socket.js file

---
+++ backend/config/socket.js
@@ -0,0 +1,30 @@
+const SocketServer = require("websocket").server;
+const http = require("http");
+
+const server = http.createServer((req, res) => {});
+
+server.listen(8000, () => {
+  console.log("Listening on port 3000...");
+});
+
+wsServer = new SocketServer({ httpServer: server });
+
+const connections = [];
+
+wsServer.on("request", (req) => {
+  const connection = req.accept();
+  console.log("new connection");
+  connections.push(connection);
+
+  connection.on("message", (mes) => {
+    connections.forEach((element) => {
+      //add another filter to only send to people with same roomId
+      if (element != connection) element.sendUTF(mes.utf8Data);
+    });
+  });
+
+  connection.on("close", (resCode, des) => {
+    console.log("connection closed");
+    connections.splice(connections.indexOf(connection), 1);
+  });
+});
