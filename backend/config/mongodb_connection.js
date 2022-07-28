cd1506068a0b5116bce631bc4d59afdb6ea5e7af
nicolas-source <nzheng8@student.ubc.ca>
2022-07-28 00:45:09 -0700

    admin access works

--- backend/config/mongodb_connection.js
+++ backend/config/mongodb_connection.js
@@ -1,5 +1,5 @@
 const { MongoClient } = require("mongodb")  // this is multiple return
-const uri = "mongodb://20.230.148.126:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.5.0"
+const uri = "mongodb://admin:shopeer@20.230.148.126:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.5.0"
 const mongoClient = new MongoClient(uri)
 const user_collection = mongoClient.db("shopeer_database").collection("user_collection")
 mongoClient.connect()
