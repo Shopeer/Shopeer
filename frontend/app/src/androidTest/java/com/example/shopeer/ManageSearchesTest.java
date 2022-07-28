bb6d4280310fbdf49371c8de6e39752a2d090117
sallyyjan <sallyyujeongan@gmail.com>
2022-07-27 19:14:56 -0700

    Manage Search Test setup and clean

--- frontend/app/src/androidTest/java/com/example/shopeer/ManageSearchesTest.java
+++ frontend/app/src/androidTest/java/com/example/shopeer/ManageSearchesTest.java
@@ -1,4 +1,137 @@
 package com.example.shopeer;
 
+import static androidx.test.InstrumentationRegistry.getTargetContext;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.fail;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.espresso.intent.Intents;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.filters.LargeTest;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+@LargeTest
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class ManageSearchesTest {
+    static Intent intent;
+    static {
+        intent = new Intent(ApplicationProvider.getApplicationContext(), MainActivity.class);
+        intent.putExtra("email", "test@email.com");
+    }
+
+    final String email = "searchTest@gmail.com";
+    final String TAG = "ManageSearch Test";
+    final String profileUrl = "http://20.230.148.126:8080/user/registration?email=";
+
+    final Context testContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+
+    @Before
+    public void testSetup() {
+        //assertEquals("com.example.volleysampleforgithub", appContext.getPackageName());
+        // setup new user
+        String url = profileUrl + email + "&name=Search Test";
+        Log.d(TAG, "POST_registration: " + url);
+        try {
+            RequestQueue requestQueue = Volley.newRequestQueue(testContext);
+            StringRequest jsonStrReq = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    Log.d(TAG, "GET_profile response: " + response);
+                    assertNotEquals(response.compareToIgnoreCase("user already exists"), 0);
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.e(TAG, "onErrorResponse POST_registration: " + error.toString());
+                    fail("Could not create new user during setup: \n" + "onErrorResponse POST_registration: " + error.toString());
+                }
+            });
+            requestQueue.add(jsonStrReq);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityScenarioRule = new ActivityScenarioRule<>(intent);
+
+    @Test // action 1
+    public void A_addSearchButton_launchNewSearch() {
+        Intents.init();
+        onView(withId(R.id.add_search_button)).perform(click());
+        intended(hasComponent(new ComponentName(getTargetContext(), EditSearchActivity.class)));
+        Intents.release();
+
+        onView(withId(R.id.search_name_text)).check(matches(withText("my search")));
+    }
+
+    @Test
+    public void B_saveSearchButton_invalidActivity() {}
+
+
+    //test spinner is on the right search
+    /*
+    https://stackoverflow.com/questions/31420839/android-espresso-check-selected-spinner-text
+     */
+
+
+    @After
+    public void testCleanup() {
+        // delete user
+        String url = profileUrl + email;
+        Log.d(TAG, "DELETE_registration: " + url);
+        try {
+            RequestQueue requestQueue = Volley.newRequestQueue(testContext);
+            StringRequest jsonStrReq = new StringRequest(Request.Method.DELETE, url, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    Log.d(TAG, "DELETE_registration response: " + response);
+                    assertEquals(response.compareToIgnoreCase("user deleted"), 0);
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.e(TAG, "onErrorResponse DELETE_registration: " + error.toString());
+                    fail("Could not delete user during setup: \n" + "onErrorResponse POST_registration: " + error.toString());
+                }
+            });
+            requestQueue.add(jsonStrReq);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
