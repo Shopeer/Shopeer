de813c93a13c6ee844222359c0f089656e4bc01c
Grace <gracemyzhang@gmail.com>
2022-07-28 15:26:27 -0700

    chats in progress

--- backend/chat/chat.test.js
+++ backend/chat/chat.test.js
@@ -3,43 +3,38 @@ const express = require('express');
 const app = express()
 // reuse original application?
 const roomsRouter = require('../chat/room');
-app.use('/chat/room', roomsRouter)
+app.use('/chat', roomsRouter)
 
 const mssgRouter = require('../chat/message');
-app.use('/chat/message', mssgRouter)
+app.use('/chat', mssgRouter)
 
 describe("Send message scenario", () => {
     const invalidRoomId = "clearlyfakeid"
     const fakeRoomId = "62e2feb74ce5451dd12322a4"
+    const testMessage = {"email": "test@mail.com", "text": "sup!", "time": "3 PM"}
 
     it('should return 404 for nonexisting room', async function () {
       // first try to delete the room, just in case
-      await request(app).delete('/').query({email: nonexistentEmail })
-      // attempt to get a nonexisting user's peerlist
-      const response = await request(app).get('/user/peers').query({ email: nonexistentEmail }).set('Accept', 'application/json')
+      await request(app).delete('/room').query({ room_id: fakeRoomId })
       
-      expect(response.body).toEqual({"response":"User not found."});
+      const response = await request(app).post('/message').query({ room_id: fakeRoomId }).send(testMessage)
+      
+      expect(response.body).toEqual({"response": "Room not found."});
       expect(response.status).toEqual(404);
     });
   
-    it('should return a peerlist sucessfully', async function () {
-      // attempt to get rob's peerlist, which should contain bob and tim
-      const response = await request(app).get('/user/peers').query({ email: emails[0] }).set('Accept', 'application/json')
-      
-      expect(response.body.length).toEqual(2);
-      expect(response.body[0].name).toEqual(names[1]);
-      expect(response.body[0].email).toEqual(emails[1]);
-      expect(response.body[1].name).toEqual(names[2]);
-      expect(response.body[1].email).toEqual(emails[2]);
-      expect(response.status).toEqual(200);
+    it('should return 400 for invalid room id', async function () {
+        
+        const response = await request(app).post('/message').query({ room_id: invalidRoomId }).send(testMessage)
+        expect(response.status).toEqual(400);
     });
   
-    it('should return an empty peerlist successfully', async function () {
-      // attempt to get Jim's peerlist
-      const response = await request(app).get('/user/peers').query({ email: emails[3] }).set('Accept', 'application/json')
+    // it('should return an empty peerlist successfully', async function () {
+    //   // attempt to get Jim's peerlist
+    //   const response = await request(app).get('/user/peers').query({ email: emails[3] }).set('Accept', 'application/json')
       
-      expect(response.body.length).toEqual(0);
-      expect(response.status).toEqual(200);
-    });
+    //   expect(response.body.length).toEqual(0);
+    //   expect(response.status).toEqual(200);
+    // });
   
   });
