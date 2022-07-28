2495d0a666be6bd0398091199c8e1c7c0404f6ef
nicolas-source <nzheng8@student.ubc.ca>
2022-07-28 00:55:11 -0700

    keys back in

--- backend/config/mongodb_connection.js
+++ backend/config/mongodb_connection.js
@@ -1,5 +1,6 @@
 const { MongoClient } = require("mongodb")  // this is multiple return
-const uri = "mongodb://"+process.env.DB_USER+":"+process.env.DB_PASS+"@20.230.148.126:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.5.0"
+const uri = "mongodb://"+"admin"+":"+"shopeer"+"@20.230.148.126:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.5.0"
+// const uri = "mongodb://"+process.env.DB_USER+":"+process.env.DB_PASS+"@20.230.148.126:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.5.0"
 const mongoClient = new MongoClient(uri)
 const user_collection = mongoClient.db("shopeer_database").collection("user_collection")
 mongoClient.connect()
