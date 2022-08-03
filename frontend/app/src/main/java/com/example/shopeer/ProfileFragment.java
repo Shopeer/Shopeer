73f1e5cadaafb3e2ae632f07632afcfe34aaacea
Farrandi Hernando <farrandi@Nandos-MacBook.local>
2022-08-03 14:22:25 -0700

    finished logout and delete account

--- frontend/app/src/main/java/com/example/shopeer/ProfileFragment.java
+++ frontend/app/src/main/java/com/example/shopeer/ProfileFragment.java
@@ -2,6 +2,8 @@ package com.example.shopeer;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
@@ -34,6 +36,8 @@ import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.tasks.OnCompleteListener;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -59,6 +63,7 @@ public class ProfileFragment extends Fragment {
     private Button deleteButton;
 
     final private String profileUrl = "http://20.230.148.126:8080/user/profile?email=";
+    final private String deleteUrl = "http://20.230.148.126:8080/user/registration?email=";
 
     private final ActivityResultLauncher<Intent> pickImage = registerForActivityResult(
             new ActivityResultContracts.StartActivityForResult(),
@@ -115,6 +120,8 @@ public class ProfileFragment extends Fragment {
         init(v);
         getProfileInfo();
         setUpdateProfile();
+        setLogoutButton();
+        setDeleteButton();
         return v;
     }
 
@@ -203,9 +210,73 @@ public class ProfileFragment extends Fragment {
         logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                GoogleSignIn.getClient()
+                GoogleSignInClient client = GoogleSignIn.getClient(getContext(), LoginActivity.gso);
+                client.signOut();
+                Toast.makeText(getActivity(), "Successfully Logged Out", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getContext(), LoginActivity.class);
+                startActivity(intent);
+
             }
         });
+    }
+
+    private void setDeleteButton() {
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                createDeleteDialog();
+            }
+        });
+    }
+    private void createDeleteDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setCancelable(true);
+        builder.setTitle("Confirm Deletion");
+        builder.setMessage("Are you sure you want to delete your account?");
+        builder.setPositiveButton("Confirm",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Log.d(TAG, "Deleting account");
+                        GoogleSignInClient client = GoogleSignIn.getClient(getContext(), LoginActivity.gso);
+                        client.signOut();
+
+                        deleteAccount();
+                    }
+                });
+        builder.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                return;
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    private void deleteAccount() {
+        try {
+            RequestQueue requestQueue = Volley.newRequestQueue(getContext());
+            String url = deleteUrl + MainActivity.email;
+            StringRequest stringRequest = new StringRequest(Request.Method.DELETE, url, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    Toast.makeText(getActivity(), "Account Successfully deleted", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(getContext(), LoginActivity.class);
+                    startActivity(intent);
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.d(TAG, "onErrorResponse login: " + error.toString());
+                }
+            });
+            requestQueue.add(stringRequest);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     private void updateProfileInBackend(String encodedImage) {
