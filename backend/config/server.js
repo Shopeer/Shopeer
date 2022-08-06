977d6cf6f26afc50ce9505afa0113c7efdfdd9ee
nicolas-source <nzheng8@student.ubc.ca>
2022-08-06 01:01:10 -0700

    fix

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,13 +1,5 @@
-// // local vm
-// const IP = '192.168.64.15';
-// azure vm
-// const IP = "20.230.148.126";
-
-
 const PORT = 8080;
-
 const app = require('./app.js')
-
 app.listen(PORT, function () {
     console.log("App running at http://:%s", PORT)
 })
