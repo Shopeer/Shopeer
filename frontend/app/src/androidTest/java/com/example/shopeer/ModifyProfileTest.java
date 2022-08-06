005683337e87e64ab0bca8b08f3b91f4da3ae118
sallyyjan <sallyyujeongan@gmail.com>
2022-08-05 23:24:57 -0700

    unused imports

--- frontend/app/src/androidTest/java/com/example/shopeer/ModifyProfileTest.java
+++ frontend/app/src/androidTest/java/com/example/shopeer/ModifyProfileTest.java
@@ -23,13 +23,6 @@ import static java.lang.Thread.sleep;
 import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
-
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.Drawable;
-import android.net.Uri;
-import android.os.Bundle;
-import android.os.Parcelable;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
--- frontend/app/src/main/java/com/example/shopeer/MyFirebaseMessagingService.java
+++ frontend/app/src/main/java/com/example/shopeer/MyFirebaseMessagingService.java
@@ -1,24 +1,10 @@
 package com.example.shopeer;
 
 import android.content.Context;
-import android.content.Intent;
 import android.util.Log;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.VolleyLog;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.UnsupportedEncodingException;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
     private static final String TAG = "MyFirebaseMsgService";
