d9614937d15676862c0075c238ab16c599247b91
nicolas-source <nzheng8@student.ubc.ca>
2022-08-06 10:50:51 -0700

    updated config

--- backend/config/mongodb_connection.js
+++ backend/config/mongodb_connection.js
@@ -1,9 +1,15 @@
-require("dotenv").config()
+if (process.env.NODE_ENV) {
+    require("dotenv").config({
+        path: `${__dirname}/../.env.${process.env.NODE_ENV}`
+    })
+} else {
+    require("dotenv").config();
+}
 const { MongoClient } = require("mongodb")
 const uri = "mongodb://"+process.env.DB_USER+":"+process.env.DB_PASS+"@20.230.148.126:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.5.0"
 const mongoClient = new MongoClient(uri)
 const user_collection = mongoClient.db(process.env.DB_DATABASE).collection("user_collection")
 const room_collection = mongoClient.db(process.env.DB_DATABASE).collection("room_collection")
 mongoClient.connect()
-
+console.log("Database: " + process.env.DB_DATABASE)
 module.exports = {user_collection, room_collection}
