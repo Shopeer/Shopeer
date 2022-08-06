47d3e8d6106b20d4a810a966b3161f057438102c
sallyyjan <sallyyujeongan@gmail.com>
2022-08-05 21:05:36 -0700

    cleaning up

--- frontend/app/src/main/java/com/example/shopeer/LoginActivity.java
+++ frontend/app/src/main/java/com/example/shopeer/LoginActivity.java
@@ -36,7 +36,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 public class LoginActivity extends AppCompatActivity {
